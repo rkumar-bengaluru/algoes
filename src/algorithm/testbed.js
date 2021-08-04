c88153a7b8853dff0b5fd821328e74d1de95cde0
rkumar-bengaluru <rupak.kumar.ambasta@gmail.com>
2021-08-04 19:36:58 +0530

    cleanup

--- src/algorithm/testbed.js
+++ src/algorithm/testbed.js
@@ -1,75 +1,78 @@
-var input = ['3', '6', '8','2'];
+var input = ['3', '6', '8', '2'];
 var output = [];
-var plus = [];
-var minus = [];
-var star = [];
-var r = 24;
+var d = [];
+var r = 53;
+var res = '';
 
-function checkIfValid(a,b,r) {
-  	console.log('f-'+ a + ',s-' + b);
-  	f = parseInt(a);
-  	s = parseInt(b);
-  	plus.push((f+s));
-  	minus.push((f-s));
-  	star.push((f*s));
-  	if( ((f+s) === r)) {
-  		return true;
-    } else if( ((f-s) === r)) {
-  		return true;
-    } else if( ((f*s) === r)) {
-  		return true;
+function checkIfValid(a, b, r) {
+    
+    f = parseInt(a);
+    s = parseInt(b);
+    d.push((f + s));
+    d.push((f - s));
+    d.push((f * s));
+    if (((f + s) === r)) {
+        console.log('f-' + a + ',s-' + b);
+        res += 'f-' + a + ',s-' + b;
+        return true;
+    } else if (((f - s) === r)) {
+        console.log('f-' + a + ',s-' + b);
+        res += 'f-' + a + ',s-' + b;
+        return true;
+    } else if (((f * s) === r)) {
+        console.log('f-' + a + ',s-' + b);
+        res += 'f-' + a + ',s-' + b;
+        return true;
     }
     return false;
 }
 
 function findDynamic(input) {
-  	var f = input[0];
-  	var s = input[1];
-  	if(checkIfValid(f,s,r)) {
-    	return true;
+    var f = input[0];
+    var s = input[1];
+    if (checkIfValid(f, s, r)) {
+        return true;
     }
-  	console.log(plus);
-  	console.log(minus);
- 	 console.log(star);
-  	for(var i = 2;i < input.length;i++) {
-      	if(checkIfValid(plus[i-1],input[i])) {
-        	return true;
+    console.log(d);
+    for (var i = 2; i < input.length; i++) {
+        for(let k of d) {
+            if (checkIfValid(k, input[i], r)) {
+                //console.log(d);
+                res += 'k-' + k + ',s-' + input[i];
+                return true;
+            }
         }
-      	if(checkIfValid(minus[i-1],input[i])) {
-        	return true;
-        }
-      	if(checkIfValid(star[i-1],input[i])) {
-        	return true;
-        }
+        
     }
-  	return false;
+    return false;
 }
 console.log(findDynamic(input));
+console.log(res);
 function permutation(input, start, end) {
-  	if(start === end) {
-    	let tmp = [];
-      	for(let c of input) {
-        	tmp.push(c);
+    if (start === end) {
+        let tmp = [];
+        for (let c of input) {
+            tmp.push(c);
         }
-      	output.push(tmp);
-      	
-      	return;
+        output.push(tmp);
+
+        return;
     }
-  	for(var i = start; i <= end;i++) {
-    	swap(input,i,start);
-      	permutation(input, start + 1, end);
-      	// backtrack
-      	swap(input,i,start);
+    for (var i = start; i <= end; i++) {
+        swap(input, i, start);
+        permutation(input, start + 1, end);
+        // backtrack
+        swap(input, i, start);
     }
 }
 
 function swap(input, i, j) {
-  	let tmp = input[i];
-  	input[i] = input[j];
-  	input[j] = tmp;
+    let tmp = input[i];
+    input[i] = input[j];
+    input[j] = tmp;
 }
 // driver code
-permutation(input, 0, input.length -1);
-console.log(JSON.stringify(output));
-console.log(output.length);
+// permutation(input, 0, input.length - 1);
+// console.log(JSON.stringify(output));
+// console.log(output.length);
 
