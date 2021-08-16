f28b0caad4b713781d02b361dcb3f0124053ff5c
rkumar-bengaluru <rupak.kumar.ambasta@gmail.com>
2021-08-16 16:58:37 +0530

    newalgoes

--- src/algorithm/testbed.js
+++ src/algorithm/testbed.js
@@ -1,110 +1,88 @@
-/**
- * Check if a given input is a palindrome or not, 
- * an input is an palindrom if the characters from
- * left and right are the same.
- * 
- * @param {palindrome} input 
- * @returns 
- */
-function checkIfPalindrome(input) {
-    input = input.toLowerCase();
-    var i = 0;
-    var j = input.length - 1;
+var A = [3, 4, -1, 0, -2, 2, 1, 7, -8];
+var a = [-2, -3];
+var b = [5, 4, 3, 2, 1]
 
-    while (j >= i) {
-        if (input[i] != input[j])
-            return false;
-        i++;
-        j--;
+console.log(A);
+var j = movenegative(A, 0, A.length - 1);
+console.log(j);
+console.log(findLeastPositive(j));
+
+
+function findLeastPositive(arr) {
+    var size = arr.length - 1;
+    
+    for (var i = 0; i <= size - 1; i++) {
+        let x = arr[i];
+        if (x - 1 < size && arr[x - 1] > 0) {
+            arr[x - 1] = -arr[x - 1];
+        }
     }
-
-    return true;
+    console.log(arr);
+    for (i = 0; i < size; i++) {
+        if (arr[i] > 0)
+            return i + 1;
+    }
+    return size + 1;
 }
-/**
- * Removes consequetive spaces in an input.
- * 
- * @param {input} input 
- * @returns 
- */
-function removeSpaceRegEx(input) {
-    var r = input.trim().replace(/\s+/g, '@');
+
+function movenegative(A, l, r) {
+    var res = [];
+    for (var i = l; i <= r; i++) {
+        if (A[i] > 0) {
+            res.push(A[i]);
+        }
+    }
+    return res;
+}
+
+function solution(A) {
+    sort(A, 0, A.length - 1);
+    console.log(A);
+    var prev = A[0];
+    for (var i = 1; i <= A.length; i++) {
+        if (A[i] - prev > 1) {
+            return prev + 1;
+        } else {
+            prev = A[i];
+        }
+    }
+    var r = A[A.length - 1] + 1;
+    if (r <= 0) {
+        return 1;
+    }
     return r;
 }
-function removeConsequetiveSpaces(input) {
-    var space = ' ';
-    var response = [];
-    for (var i = 0; i <= input.length - 1; i++) {
-        if (input[i] != space) {
-            response.push(input[i]);
+
+function merge(A, l, m, r) {
+    var i = l;
+    var j = m + 1;
+    while (i <= m && j <= r) {
+        if (A[i] > A[j]) {
+            swap(A, i, j);
+            i++; j++;
+            for (var k = j; k <= r; k++) {
+                if (A[k - 1] > A[k]) {
+                    swap(A, k - 1, k);
+                }
+            }
+            j = m + 1;
         } else {
-            if (response[response.length - 1] != space) {
-                response.push(space);
-            }
+            i++;
         }
     }
-    return response.join();
+    console.log('l-' + l + ',m-' + m + ',r-' + r);
+    console.log(A);
 }
-/**
- * Check if the given input is a parangram, an input
- * is a panagram if the input contains all characters
- * from a --> z, otherwise the input is not a paragram.
- */
-function checkIfPanagram(input) {
-    input = input.toLowerCase();
-    // a --> 97 , z --> 122 = 25
-    var a = new Array(26).fill(0);
-    for (let c of input) {
-        if (c != ' ') {
-            a[c.charCodeAt() - 97] = 1;
-        }
+function sort(A, l, r) {
+    if (l < r) {
+        var m = l + Math.floor((r - l) / 2);
+        sort(A, l, m);
+        sort(A, m + 1, r);
+        merge(A, l, m, r);
     }
-    console.log(a);
-    for (let i of a) {
-        if (i === 0)
-            return false;
-    }
-    return true;
 }
-
-function findHighFrequencyChac(input) {
-    var hf = new Array(26).fill(0);
-    input = input.toLowerCase();
-
-    for (let c of input) {
-        if (c.charCodeAt() > 96 && c.charCodeAt() <= 122) {
-            if (hf[c.charCodeAt() - 97][0] != undefined) {
-                hf[c.charCodeAt() - 97] = [hf[c.charCodeAt() - 97][0] + 1, c];
-            } else {
-                hf[c.charCodeAt() - 97] = [1, c]
-            }
-        }
-    }
-    var hfNew = [];
-    for (let h of hf) {
-        if (h != 0) {
-            hfNew.push(h);
-        }
-    }
-
-    return hfNew.sort();
+function swap(A, i, j) {
+    var t = A[i];
+    A[i] = A[j];
+    A[j] = t;
 }
-
-function rearrange(input) {
-    var hf = findHighFrequencyChac(input);
-    return String.fromCharCode(97 + MAXINDEX);
-}
-
-function nearestWhole(n) {
-
-    let a = parseInt(n / 10) * 10;
-    let b = a + 10;
-    return (n - a > b - n)? b : a;
-}
-
-console.log(checkIfPalindrome('iTopiNonAvevanoNipoti'));
-console.log(removeSpaceRegEx('r  u pak'));
-console.log(removeConsequetiveSpaces('r  u pak'));
-console.log(checkIfPanagram('The quick brown fox jumps over the lazy dog'));
-console.log(findHighFrequencyChac('aaabbbcc '));
-console.log(nearestWhole(4722));
-console.log(nearestWhole(38));
