753d11d3b3b646d46be87f5416801cafa0f19171
rkumar-bengaluru <rupak.kumar.ambasta@gmail.com>
2021-08-17 16:19:36 +0530

    sitemap

--- src/algorithm/testbed.js
+++ src/algorithm/testbed.js
@@ -1,88 +1,44 @@
-var A = [3, 4, -1, 0, -2, 2, 1, 7, -8];
-var a = [-2, -3];
-var b = [5, 4, 3, 2, 1]
-
-console.log(A);
-var j = movenegative(A, 0, A.length - 1);
-console.log(j);
-console.log(findLeastPositive(j));
-
-
-function findLeastPositive(arr) {
-    var size = arr.length - 1;
-    
-    for (var i = 0; i <= size - 1; i++) {
-        let x = arr[i];
-        if (x - 1 < size && arr[x - 1] > 0) {
-            arr[x - 1] = -arr[x - 1];
-        }
+/**
+ * Write a function
+ *
+ * that, given an array A consisting of N integers, returns the number of 
+ * distinct values in array A. For example, given array A consisting of 
+ * six elements such that:
+ *
+ * A[0] = 2    A[1] = 1    A[2] = 1
+ * A[3] = 2    A[4] = 3    A[5] = 1
+ * 
+ * the function should return 3, because there are 3 distinct values 
+ * appearing in array A, namely 1, 2 and 3.
+ *
+ * Write an efficient algorithm for the following assumptions:
+ *
+ */
+ function solution(A) {
+    if (A.length === 0) {
+        return 0;
     }
-    console.log(arr);
-    for (i = 0; i < size; i++) {
-        if (arr[i] > 0)
-            return i + 1;
-    }
-    return size + 1;
-}
-
-function movenegative(A, l, r) {
-    var res = [];
-    for (var i = l; i <= r; i++) {
-        if (A[i] > 0) {
-            res.push(A[i]);
-        }
-    }
-    return res;
-}
-
-function solution(A) {
-    sort(A, 0, A.length - 1);
-    console.log(A);
-    var prev = A[0];
-    for (var i = 1; i <= A.length; i++) {
-        if (A[i] - prev > 1) {
-            return prev + 1;
+    var map = new Map();
+    for (let c of A) {
+        if (!map.get(c)) {
+            map.set(c, 1);
         } else {
-            prev = A[i];
+            map.set(c, map.get(c) + 1);
         }
     }
-    var r = A[A.length - 1] + 1;
-    if (r <= 0) {
-        return 1;
-    }
-    return r;
+    var keys = map.keys();
+    console.log(map.size);
+    return map.size;
 }
 
-function merge(A, l, m, r) {
-    var i = l;
-    var j = m + 1;
-    while (i <= m && j <= r) {
-        if (A[i] > A[j]) {
-            swap(A, i, j);
-            i++; j++;
-            for (var k = j; k <= r; k++) {
-                if (A[k - 1] > A[k]) {
-                    swap(A, k - 1, k);
-                }
-            }
-            j = m + 1;
-        } else {
-            i++;
-        }
+console.log(solution(generate_randon(20)));
+
+function generate_randon(size) {
+    var a = new Array(size).fill(0);
+    console.log(a);
+    for (var i = 0; i < size; i++) {
+        a[i] = Math.floor(Math.random() * 101);
     }
-    console.log('l-' + l + ',m-' + m + ',r-' + r);
-    console.log(A);
-}
-function sort(A, l, r) {
-    if (l < r) {
-        var m = l + Math.floor((r - l) / 2);
-        sort(A, l, m);
-        sort(A, m + 1, r);
-        merge(A, l, m, r);
-    }
-}
-function swap(A, i, j) {
-    var t = A[i];
-    A[i] = A[j];
-    A[j] = t;
+    console.log(a);
+    return a;
 }
