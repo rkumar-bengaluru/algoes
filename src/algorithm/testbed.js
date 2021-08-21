2f7e61725a401392a254689127849553274ab484
rkumar-bengaluru <rupak.kumar.ambasta@gmail.com>
2021-08-21 11:48:00 +0530

    algoes

--- src/algorithm/testbed.js
+++ src/algorithm/testbed.js
@@ -1,44 +1,27 @@
 /**
- * Write a function
- *
- * that, given an array A consisting of N integers, returns the number of 
- * distinct values in array A. For example, given array A consisting of 
- * six elements such that:
- *
- * A[0] = 2    A[1] = 1    A[2] = 1
- * A[3] = 2    A[4] = 3    A[5] = 1
- * 
- * the function should return 3, because there are 3 distinct values 
- * appearing in array A, namely 1, 2 and 3.
- *
- * Write an efficient algorithm for the following assumptions:
- *
+ * How many ways can you make change with coins and a total amount.
+ * var Denomination = [1,2,5]
+ * var Change = 7;
  */
- function solution(A) {
-    if (A.length === 0) {
+
+var bills = [1, 2, 3];
+var change = 4;
+var ways = 0;
+var r = [];
+
+function count(A, T, n) {
+    if (T === 0) {
+        return 1;
+    }
+
+    if (n < 0 || T < 0) {
         return 0;
     }
-    var map = new Map();
-    for (let c of A) {
-        if (!map.get(c)) {
-            map.set(c, 1);
-        } else {
-            map.set(c, map.get(c) + 1);
-        }
-    }
-    var keys = map.keys();
-    console.log(map.size);
-    return map.size;
-}
+    var include = count(A, T - A[n], n);
+    var exclude = count(A, T, n - 1);
+    console.log(include + ',' + exclude);
+    return include + exclude;
 
-console.log(solution(generate_randon(20)));
-
-function generate_randon(size) {
-    var a = new Array(size).fill(0);
-    console.log(a);
-    for (var i = 0; i < size; i++) {
-        a[i] = Math.floor(Math.random() * 101);
-    }
-    console.log(a);
-    return a;
 }
+var ways = count(bills, change, bills.length - 1);
+console.log(r);
