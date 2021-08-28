8004878d8651a86dfad324dd75e7f4554f05ac62
rkumar-bengaluru <rupak.kumar.ambasta@gmail.com>
2021-08-28 11:19:46 +0530

    passingcars

--- src/algorithm/testbed.js
+++ src/algorithm/testbed.js
@@ -1,50 +1,35 @@
 /**
- * Given a string, a partitioning of the string is a palindrome 
- * partitioning if every substring of the partition is a 
- * palindrome. For example, “aba|b|bbabb|a|b|aba” is a palindrome 
- * partitioning of “ababbbabbababa”. Determine the fewest cuts 
- * needed for a palindrome partitioning of a given string. 
- * For example, minimum of 3 cuts are needed for “ababbbabbababa”. 
- * The three cuts are “a|babbbab|b|ababa”. If a string is a 
- * palindrome, then minimum 0 cuts are needed. If a string of 
- * length n containing all different characters, 
- * then minimum n-1 cuts are needed. 
+ * A non-empty array A consisting of N integers is given. 
+ * The consecutive elements of array A represent consecutive 
+ * cars on a road.
+ * The goal is to count passing cars. We say that a pair of 
+ * cars (P, Q), where 0 ≤ P < Q < N, is passing when P is 
+ * traveling to the east and Q is traveling to the west.
+ * For example, consider array A such that:
+ *
+ * A[0] = 0
+ * A[1] = 1
+ * A[2] = 0
+ * A[3] = 1
+ * A[4] = 1
+ * We have five pairs of passing cars: (0, 1), (0, 3), (0, 4), (2, 3), (2, 4).
  */
-var A = "ababbbabbababa";
-console.log(minPalPartion(A));
-function minPalPartion(A) {
-    var chars = [...A];
-    //console.log(chars);
-    var start = 0;
-    var end = A.length - 1;
-    var count = 0;
-    while (end > start) {
-        var l = findLongestMatchingPalindrome(A, start, end);
-        if (l > 0) {
-            //console.log(A.substring(l, end + 1));
-            start = 0;
-            end = l - 1;
-            count++;
-        } else {
-            start = start + 1;
-        }
-    }
-    return count;
-}
 
-function findLongestMatchingPalindrome(A, l, r) {
-    if (l < r) {
-        var i = l;
-        var j = r;
-        while (i < j) {
-            if (A[i] != A[j]) {
-                //console.log(i + ':-' + j + ':' + A[i] + ':' + A[j]);
-                return findLongestMatchingPalindrome(A, l + 1, r);
-            } else {
-                //console.log(i + ':' + j + ':' + A[i] + ':' + A[j]);
-                i++; j--;
-            }
+var A = [0, 1, 0, 1, 1];
+console.log(A[0].toFixed(0));
+function findPassingCars(A) {
+    var counter = 0;
+    var factor = 1;
+    var dir = A[0].toFixed(0);
+    for (var i = 1; i <= A.length - 1; i++) {
+        if (A[i].toFixed(0) === dir) {
+            factor = factor + 1;
+        } else {
+            counter = counter + factor * 1;
         }
-        return l;
     }
+    if (counter > 1000000000) {
+        return -1;
+    }
+    return counter;
 }
