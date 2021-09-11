e74e82fb05dd0ac93838661ebdfdbc63055464f7
rkumar-bengaluru <rupak.kumar.ambasta@gmail.com>
2021-09-11 13:23:18 +0530

    streamdemo

--- src/algorithm/testbed.js
+++ src/algorithm/testbed.js
@@ -1,42 +1,48 @@
-// React is loaded and is available as React and ReactDOM
-// imports should NOT be used
-// https://app.testdome.com/t
-const TodoItem = (props) => <li onClick={props.onClick}>{props.item.text}</li>
-
-class TodoList extends React.Component {
-  render() {
-    const { items, onListClick } = this.props;
-    return (<ul onClick={onListClick}>
-      {items.map((item, index) => 
-                 <TodoItem item={item} key={index} onClick={this.handleItemClick.bind(this, item)}/>)}
-    </ul>);
+const fs = require('fs');
+var stream = fs.createWriteStream('products.json', {flags: 'a'});
+/**
+     * Demonstration of async await with a real world example of 
+     * scanning a dir with millions of files. read the json content 
+     * of each file and print the sku of the file sequentially.
+     * 
+     * @param {large dir to scan} dir 
+     */
+ const scanDir = async (dir) => {
+  console.log('start scanning');
+  // await with promise on reading the 
+  // files in the destination directory
+  var files = await new Promise((resolve, reject) => {
+      fs.readdir(dir, (err, files) => {
+          if (err) {
+              reject(files);
+          }
+          resolve(files);
+      });
+  });
+  console.log('there are ' + files.length + ' in the dir');
+  stream.write('[');
+  // one approach if for each file sequentially the content.
+  for (var i = 0; i <= files.length - 1; i++) {
+      // another await inside async function to read the 
+      // content of file and return sku.
+      let sku = await new Promise((resolve, reject) => {
+          fs.readFile(dir + '\\' + files[i], (err, data) => {
+              if (err) {
+                  reject(files);
+              }
+              var p = JSON.parse(data);
+              stream.write(JSON.stringify({ "id": dir + '\\' + files[i], "title": "", "description": "", "aggregateRating": {}, "image": '' }));
+              stream.write(',');
+              resolve(p.sku);
+          });
+      });
+      // sequentially print each sku
+      console.log('sku of the product is ' + sku);
   }
-  
-  handleItemClick(item, event) {
-    // Write your code here
-    event.preventDefault();
-    if(!item.done) {
-      item.done = true;
-    }
-  }
+  stream.write(']');
+  stream.end();
+  // finally scanning is complete.
+  console.log('scanning done...');
 }
 
-
-const items = [ { text: 'Buy grocery', done: true },
-  { text: 'Play guitar', done: false },
-  { text: 'Romantic dinner', done: false }
-];
-
-const App = (props) => <TodoList
-  items={props.items}
-  onListClick={(event) => console.log("List clicked!")}
-  onItemClick={(item, event) => { console.log(item, event); 
-                                if(!item.done) {
-                                  this.handleItemClick(item,event);
-                                }
-                                }}
-/>;
-
-document.body.innerHTML = "<div id='root'></div>";
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App items={items}/>, rootElement);
+scanDir('E:\\2021\\localshop\\data\\catalog\\grocery\\catalog\\products-01');
--- src/java8/stream.js
+++ src/java8/stream.js
@@ -11,13 +11,81 @@ const Java8Stream = (props) => {
     const [countDemo, setCountDemo] = useState([5, 2, 6, 1, 0]);
     const [showCount, setShowCount] = useState(false);
     const [totalCount, setTotalCount] = useState(5);
+    //
+    const [intermediate, setIntermediate] = useState('filter');
+    const [terminal, setTerminal] = useState('forEach');
+    const [ilambdas, setIlamdas] = useState([]);
+    const [tlambdas, setTlamdas] = useState([]);
+    const [iexpression, setIexpression] = useState(0);
+    const [texpression, setTexpression] = useState(9);
+    const [intermediateStream, setIntermediateStream] = useState([]);
+    const [terminalResponse, setTerminalResponse] = useState([]);
+
+    const allLambdas = [
+        {
+            id: 0, value: "Predicate<T> p = (t) = > { t > 49}", type: 'predicate'
+        },
+        {
+            id: 1, value: "Predicate<T> p = (t) = { t < 49}", type: 'predicate'
+        },
+        {
+            id: 2, value: "Function<T,R> f = (t) = { Arrays.asList(t,t*t);}", type: 'function'
+        },
+        {
+            id: 3, value: "1", type: 'number'
+        },
+        {
+            id: 4, value: "2", type: 'number'
+        },
+        {
+            id: 5, value: "3", type: 'number'
+        },
+        {
+            id: 6, value: "4", type: 'number'
+        },
+        {
+            id: 7, value: "", type: 'sorted'
+        },
+        {
+            id: 8, value: "Comparator<T> c = ", type: 'compare'
+        },
+        {
+            id: 9, value: "Consumer<T> c = (t) => {System.out.println(t)}", type: 'consumer'
+        },
+        {
+            id: 8, value: "Consumer<T> c = ", type: 'consumer'
+        },
+        {
+            id: 10, value: "Comparator<T> c = ", type: 'compare'
+        },
+        {
+            id: 11, value: "Comparator<T> c = ", type: 'compare'
+        }
+    ]
 
     useEffect(() => {
-        cm.current.editor.display.wrapper.style.height = "200px";
+        //cm.current.editor.display.wrapper.style.height = "200px";
+        //cm.current.editor.display.wrapper.style.width = "200px";
         setCountCode('import java.util.stream.Stream;\n\npublic class StreamCountExample {\n\n\tpublic static void main(String[] args) {\n\t\t// create a new stream\n\t\tStream < Integer > s = Stream.of(5, 2, 6, 1, 0);\n\t\t// count no of elements in stream;\n\t\tlong noOfElements = s.count();\n\t\tSystem.out.println(noOfElements);\n\t}\n}\n');
         setCountDemo([5, 2, 6, 1, 0]);
         setTotalCount(5);
+        setIntermediate('filter');
+        setTerminal('forEach');
+        var functions = allLambdas.filter((lambda, index) => {
+            return lambda.type === 'predicate'
+        });
+        setIlamdas(functions);
+        var functions = allLambdas.filter((lambda, index) => {
+            return lambda.type === 'consumer'
+        });
+        setTlamdas(functions);
+        setIexpression(0);
+        setTexpression(9);
+        setIntermediateStream([]);
+        setTerminalResponse([]);
     }, [countCode]);
+
+
 
     function countClick() {
         setShowCount(!showCount);
@@ -29,14 +97,16 @@ const Java8Stream = (props) => {
         return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
     }
 
-    function createRandomStream() {
-        var howMany = getRandomInt(2, 10);
+    function createRandomStream(min, max) {
+        var howMany = getRandomInt(min, max);
         var nos = [];
         for (var i = 0; i <= howMany; i++) {
             nos.push(getRandomInt(0, 100));
         }
         setCountDemo(nos);
         setTotalCount(nos.length);
+        setIntermediateStream([]);
+        setTerminalResponse([]);
     }
 
     function msort(A, l, r) {
@@ -78,17 +148,88 @@ const Java8Stream = (props) => {
         var data = [...countDemo];
         msort(data, 0, data.length - 1);
         setCountDemo(data);
-        // var func = setInterval(function () {
+    }
+    // select intermediate operation event.
+    function handleIntermediateFunctionChange(e) {
+        setIntermediate(e.target.value);
+        if (e.target.value === 'filter') {
+            var functions = allLambdas.filter((lambda, index) => {
+                return lambda.type === 'predicate'
+            });
+            setIlamdas(functions);
+        } else if (e.target.value === 'map' || e.target.value === 'flatMap') {
+            var functions = allLambdas.filter((lambda, index) => {
+                return lambda.type === 'function'
+            });
+            setIlamdas(functions);
+        } else if (e.target.value === 'distinct') {
+            setIlamdas([]);
+        } else if (e.target.value === 'limit' || e.target.value === 'skip') {
+            var functions = allLambdas.filter((lambda, index) => {
+                return lambda.type === 'number'
+            });
+            setIlamdas(functions);
+        } else if (e.target.value === 'compare') {
+            var functions = allLambdas.filter((lambda, index) => {
+                return lambda.type === 'compare'
+            });
+            setIlamdas(functions);
+        }
+    }
+    function handleTerminalOperationChange(e) {
+        setTerminal(e.target.value);
+        if (e.target.value === 'forEach' || e.target.value === 'forEachOrdered') {
+            var functions = allLambdas.filter((lambda, index) => {
+                return lambda.type === 'consumer'
+            });
+            setTlamdas(functions);
+        } else if (e.target.value === 'min' || e.target.value === 'max') {
+            var functions = allLambdas.filter((lambda, index) => {
+                return lambda.type === 'compare'
+            });
+            setTlamdas(functions);
+        }
+    }
+    function expressionIChange(e) {
+        setIexpression(e.target.value);
+        console.log('expression changed ' + e.target.value);
+    }
+    function expressionTChange(e) {
+        setTexpression(e.target.value);
+        console.log('expression changed ' + e.target.value);
+    }
 
-        // }, 3000);
+    function applyInterMediateStep() {
+        if (parseInt(iexpression) === 0) {
+            var intermediateStream = countDemo.filter((p) => {
+                return p > 49;
+            });
+            setIntermediateStream(intermediateStream);
+        } else if (parseInt(iexpression) === 1) {
+            var intermediateStream = countDemo.filter((p) => {
+                return p < 49;
+            });
+            setIntermediateStream(intermediateStream);
+        }
+    }
+    function applyTerminalStep() {
+        console.log(texpression);
+        var res = [];
+        if (parseInt(texpression) === 9) {
+            intermediateStream.map((e) => {
+                console.log(e);
+                res.push(e);
+                setTerminalResponse(res);
+            });
+        }
     }
 
     return (
         <div className="container-fluid">
             <h1>Java 8 Stream</h1>
-            <h2 className="m-3">Java Stream count()</h2>
+            <h2 className="m-3">Java Stream Demo</h2>
             <div className="row border">
-                <div className="col-sm-6">
+                {/* <div className="col-sm-4">
                     <CodeMirror
                         value={countCode}
                         ref={cm}
@@ -101,9 +242,9 @@ const Java8Stream = (props) => {
                             mode: "javascript"
                         }}
                     />
-                </div>
-                <div className="col-sm-6">
-                    <div className="row m-2">
+                </div> */}
+                <div className="col-sm-12">
+                    {/* <div className="row m-2">
                         {countDemo.map((element, index) => {
                             return (
                                 <div key={index} className="col border">
@@ -114,7 +255,7 @@ const Java8Stream = (props) => {
                     </div>
                     <div className="row m-2">
                         <div className="col-sm-4">
-                            <button onClick={() => createRandomStream()}>Create Stream</button>
+                            <button onClick={() => createRandomStream(2, 10)}>Create Stream</button>
                         </div>
                         <div className="col-sm-4">
                             <button onClick={() => countClick()}>Show Count</button>
@@ -127,6 +268,120 @@ const Java8Stream = (props) => {
                         {showCount &&
                             <div className="col-sm-12" style={{ color: "red" }}>Stream Count = {totalCount}</div>
                         }
+                    </div> */}
+                    <div className="border">
+                        <div className="row m-2">
+                            <button onClick={() => createRandomStream(10, 50)}>Create Random Stream</button>
+                        </div>
+                        <div className="row m-2">
+                            {countDemo.map((element, index) => {
+                                return (
+                                    <div key={index} className="col border">
+                                        {element}
+                                    </div>
+                                )
+                            })}
+                        </div>
+                        <div className="row m-2">
+                            <div className="col-sm-3 border p-2">
+                                <span className="h6">Select Intermediate Operation</span>
+                                <div>
+                                    <select value={intermediate}
+                                        onChange={handleIntermediateFunctionChange.bind(this)} className="form-control form-control-sm">
+                                        <option value="map">map()</option>
+                                        <option value="map">flatMap()</option>
+                                        <option value="filter">filter()</option>
+                                        <option value="distinct">distinct()</option>
+                                        <option value="sorted">sorted()</option>
+                                        <option value="limit">limit()</option>
+                                        <option value="skip">skip()</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div className="col-sm-3 border p-2">
+                                <span className="h6">Select Lamda </span>
+                                <div>
+                                    <select value={iexpression} onChange={expressionIChange.bind(this)}
+                                        className="form-control-sm">
+                                        {ilambdas.map((p, i) => {
+                                            return (
+                                                <option key={i} value={p.id}>{p.value}</option>
+                                            )
+                                        })}
+                                    </select>
+                                </div>
+                            </div>
+                            <div className="cold-sm-3 border p-2">
+                                <span className="h6">Select Terminal Operation</span>
+                                <div>
+                                    <select value={terminal} onChange={handleTerminalOperationChange.bind(this)}
+                                        className="form-control-sm">
+                                        <option value="forEach">forEach()</option>
+                                        <option value="forEachOrdered">forEachOrdered()</option>
+                                        <option value="count">count()</option>
+                                        <option value="min">min()</option>
+                                        <option value="max">max()</option>
+                                        <option value="collect">collect()</option>
+                                        <option value="reduce">reduce()</option>
+                                        <option value="toArray">toArray()</option>
+                                        <option value="anyMatch">anyMatch()</option>
+                                        <option value="allMatch">allMatch()</option>
+                                        <option value="noneMatch">noneMatch()</option>
+                                        <option value="findFirst">findFirst()</option>
+                                        <option value="findAny">findAny()</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div className="col-sm-3 border p-2">
+                                <span className="h6">Select Lamda </span>
+                                <div>
+                                    <select value={texpression} onChange={expressionTChange.bind(this)}
+                                        className="form-control-sm">
+                                        {tlambdas.map((p, i) => {
+                                            return (
+                                                <option key={i} value={p.id}>{p.value}</option>
+                                            )
+                                        })}
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row p-2 border">
+                            <div className="col-sm-6 border p-2">
+                                <span className="h6">Intermediate Operation Result</span>
+                                <div className="row m-2">
+                                    <button onClick={() => applyInterMediateStep()}>Apply InterMediate Step</button>
+                                </div>
+                                <div className="row">
+                                    {intermediateStream.map((d, index) => {
+                                        return (
+                                            <span key={index} className="col border">{d}</span>
+                                        )
+                                    })}
+                                    {intermediateStream.length <= 0 &&
+                                        <div>no data in stream</div>
+                                    }
+                                </div>
+                            </div>
+                            <div className="col-sm-6 border p-2">
+                                <span className="h6">Terminal Operation Result</span>
+                                <div className="row m-2">
+                                    <button onClick={() => applyTerminalStep()}>Apply Terminal Step</button>
+                                </div>
+                                <div className="row m-2">
+                                    <div className="col-sm-12">
+                                        {terminalResponse.map((d, index) => {
+                                            return (
+                                                <div key={index} className="row">{d}</div>
+                                            )
+                                        })}
+                                        {terminalResponse.length <= 0 &&
+                                            <div>no data in terminal</div>
+                                        }
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
