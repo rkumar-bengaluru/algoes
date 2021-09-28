88b31511648f6fbbddc5a13de646977ad220ab44
rkumar-bengaluru <rupak.kumar.ambasta@gmail.com>
2021-09-28 12:10:18 +0530

    spring

--- src/spring/home.js
+++ src/spring/home.js
@@ -1,9 +1,23 @@
 const SpringIntro = () => {
     return (
         <div>
-            <h6>Spring Home</h6>
+            <h6>Spring Boot</h6>
+            <p>
+                We are going to build an application using various features of <a href="https://spring.io/" target="_blank">Spring.io</a>. 
+            </p>
+            <h6>Design Overview</h6>
             <div className="row">
-                TODO
+                <div className="col"><img width="500" src='/algoes/img/spring.io/p.jpg'></img></div>
+                <div className="col">
+                    <ul>
+                        <li>@LocalShopApplication - Springboot application</li>
+                        <li>@OrderController &amp; @AnalyticsController - RESTful Web Service</li>
+                        <li>@OrderService &amp; @AnalyticsService - Business Service Facade</li>
+                        <li>@RestTemplate - Rest Client</li>
+                        <li>@WebFlux - sync, async, and streaming scenarios </li>
+                        <li>@LocalShopRepository - @Repository to access data </li>
+                    </ul>
+                </div>
             </div>
         </div>
     )
