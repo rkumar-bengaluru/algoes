f86c44a012cd6341b1719a1b3cab4daa8bec5370
rkumar-bengaluru <rupak.kumar.ambasta@gmail.com>
2021-10-14 18:59:53 +0530

    Updates

---
+++ static/js/16.af86923a.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpalgoes=this.webpackJsonpalgoes||[]).push([[16],{103:function(e,s,t){"use strict";t.r(s);var c=t(0);s.default=function(){return Object(c.jsxs)("div",{className:"container",children:[Object(c.jsx)("h3",{children:"Redux"}),Object(c.jsx)("p",{children:"A Predictable State Container for JS Apps"}),Object(c.jsx)("p",{children:"Work In Progress"})]})}}}]);
+//# sourceMappingURL=16.af86923a.chunk.js.map
